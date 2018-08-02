4599927f9229b8841dc7636c86787a471f2ee355
alex-avila <alex.avilx@icloud.com>
2018-08-01 21:36:26 -0600

    Test npm run lint. It works.

--- src/components/GameGrid/index.js
+++ src/components/GameGrid/index.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 
 class GameGrid extends Component {
-    render() {
-        return (
-            <div className="grid__container">
-                <section id="grid">
-                    {this.props.gridShuffled}
-                </section>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="grid__container">
+        <section id="grid">{this.props.gridShuffled}</section>
+      </div>
+    )
+  }
 }
 
 export default GameGrid
--- src/components/WinModal/index.js
+++ src/components/WinModal/index.js
@@ -1,39 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class WinModal extends Component {
-    render() {
-        let className
-        if (this.props.isModalOn) {
-            className = 'modal fade-in'
-        } else {
-            className = 'modal'
-        }
-        const { arrowIcon } = this.props
-        const arrowIconStyle = {
-            backgroundImage: `url(${arrowIcon})`,
-            backgroundSize: 'contain',
-            height: 24,
-            width: 24,
-            cursor: 'pointer'
-        }
-        return (
-            <div onClick={this.props.handleHideModal} id="modal" className={className}>
-                <div className="modal__content">
-                    <h3>YOU WIN</h3>
-                    <p>You won in <span id="time-result">{this.props.timeResultText}</span> with <span id="star-result">{this.props.starResultText}</span> stars</p>
-                    <button id="play-again" onClick={
-                        e => {
-                            this.props.handleHideModal(e)
-                            setTimeout(this.props.reset, 500)
-                        }
-                    }>
-                        PLAY AGAIN
-                        <div style={arrowIconStyle}></div>
-                    </button>
-                </div>
-            </div>
-        );
+  render() {
+    let className
+    if (this.props.isModalOn) {
+      className = 'modal fade-in'
+    } else {
+      className = 'modal'
     }
+    const { arrowIcon } = this.props
+    const arrowIconStyle = {
+      backgroundImage: `url(${arrowIcon})`,
+      backgroundSize: 'contain',
+      height: 24,
+      width: 24,
+      cursor: 'pointer'
+    }
+    return (
+      <div
+        onClick={this.props.handleHideModal}
+        id="modal"
+        className={className}
+      >
+        <div className="modal__content">
+          <h3>YOU WIN</h3>
+          <p>
+            You won in <span id="time-result">{this.props.timeResultText}</span>{' '}
+            with <span id="star-result">{this.props.starResultText}</span> stars
+          </p>
+          <button
+            id="play-again"
+            onClick={e => {
+              this.props.handleHideModal(e)
+              setTimeout(this.props.reset, 500)
+            }}
+          >
+            PLAY AGAIN
+            <div style={arrowIconStyle} />
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default WinModal;
+export default WinModal
